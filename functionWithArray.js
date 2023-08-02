8d9898ad56957a9c6f83c3a41cd1e3fd8ad1c7e1
AlgoWebBot <shimulzahan636@gmail.com>
2023-08-02 17:15:34 +0600

    average add

--- functionWithArray.js
+++ functionWithArray.js
@@ -1,14 +1,17 @@
 
-// function add(arr, length) {
-//     var sum = 0;
-//     for (var i = 0; i < length; i++){
-//         sum += arr[i];
-//     }
-//     console.log(sum);
-// }
-// const myArray = [3, 2, 1, 4, 5, 6];
-// const arrLength = myArray.length;
-// add(myArray, arrLength);
+function add(arr, length, sum) {
+    var sum = 0;
+    for (var i = 0; i < length; i++){
+        sum += arr[i];
+    }
+    return (sum / length).toFixed(2);
+}
+var sum = 0;
+const myArray = [3, 2, 1, 4, 5, 6];
+const arrLength = myArray.length;
+var avg = add(myArray, arrLength);
+console.log(avg);
+
 
 
 // // function in a function
@@ -25,15 +28,15 @@
 
 
 // function odd-even
-function odd(x) {
-    if (x % 2 != 0) {
-        console.log('Odd Number');
-    } else {
-        even();
-    } 
-} function even() {
-    console.log('Even Number');
-}
+// function odd(x) {
+//     if (x % 2 != 0) {
+//         console.log('Odd Number');
+//     } else {
+//         even();
+//     } 
+// } function even() {
+//     console.log('Even Number');
+// }
 
-const number = 5;
-odd(number);
+// const number = 5;
+// odd(number);
